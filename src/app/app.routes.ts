a65413a25b2c8a13928ce6d52686d1c07e233d04
AmonMcDuul <joranomic@hotmail.com>
2024-04-26 20:33:58 +0200

    start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GameComponent } from './pages/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+    {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+    title: 'Game',
+  },
+];
