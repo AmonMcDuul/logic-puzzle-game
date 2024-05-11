a11f7e37ddfcee3f8b4b90b0da19a1a3ef5de19f
AmonMcDuul <joranomic@hotmail.com>
2024-05-11 22:22:02 +0200

    mouse trail ha

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  trails: { x: number, y: number }[] = [];
   
+
+  constructor(private router: Router) {}
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.trails.push({ x: event.clientX, y: event.clientY });
+    if (this.trails.length > 20) {
+      this.trails.shift();
+    }
+  }
   newGame(gameType: string) {
     this.router.navigate(['/game', { type: gameType }]);
   }
